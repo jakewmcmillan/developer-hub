0a5955899efe4b7297e08f48202c965bf1a04472
Sean Dillon <pdillon0@gmail.com>
2022-09-28 11:14:23 -0400

    saving

---
+++ server/models/Category.js
@@ -0,0 +1,8 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const categorySchema = new Category({
+
+});
+
+const Category = model('Category', categorySchema);
---
+++ server/models/Snippet.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const snippetSchema = new Schema({
+    snippetText: {
+      type: String,
+      required: 'Your code needs more code!',
+      minlength: 1,
+      maxlength: 500,
+      trim: true,
+    },
+    snippetAuthor: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    comments: [
+      {
+        commentText: {
+          type: String,
+          required: true,
+          minlength: 1,
+          maxlength: 280,
+        },
+        commentAuthor: {
+          type: String,
+          required: true,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+          get: (timestamp) => dateFormat(timestamp),
+        },
+      },
+    ],
+  });
+  
+  const Snippet = model('Snippet', snippetSchema);
+  
+  module.exports = Snippet;
