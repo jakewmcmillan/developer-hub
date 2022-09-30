0006e148a2280772003cda828813e6469be6d44f
Jake McMillan <jakewmcmillan@gmail.com>
2022-09-30 16:14:18 -0400

    Updates

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -56,24 +56,3 @@ export const ADD_COMMENT = gql`
   }
 `;
 
-export const REMOVE_SNIPPET = gql`
-  mutation removeSnippet($snippetId: ID!) {
-    removeSnippet($snippetId: ID!) {
-      _id
-      snippetText
-      snippetAuthor
-      createdAt
-      category
-      comments {
-        _id
-        commentText
-      }
-    }
-  }
-`;
-
-export const REMOVE_COMMENT = gql`
-  mutation removeComment($snippetId: ID!, commentId: ID!) {
-    removeComment($)
-  }
-`;
