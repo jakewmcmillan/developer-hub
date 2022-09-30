d2b4cba9be4dab381bf18b873fec6acd34bfec09
Sean Dillon <pdillon0@gmail.com>
2022-09-30 11:44:33 -0400

    more seeds

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -32,6 +32,53 @@ db.once('open', async () => {
           </html>`,
           "snippetAuthor":"Sean",
           "category": categories[0]._id
+      },
+      {
+        "snippetText": `
+        <!DOCTYPE html>
+<html lang="en">
+  
+<head>
+  <meta charset="UTF-8" />
+  <title>jQuery DOM elements</title>
+  <link rel="stylesheet" href="./assets/css/jass.css" />
+  <link rel="stylesheet" href="./assets/css/style.css" />
+
+</head>
+
+<body class="bg-dark text-light text-center">
+  <!-- This empty <div> will anchor the elements we create with jQuery. -->
+  <div class="m-5" id="root"></div>
+
+  <!-- Added link to the jQuery library -->
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+  <script type="text/javascript" src="./assets/js/script.js"></script>
+</body>
+
+</html>
+        `,
+        "snippetAuthor":"Sean",
+        "category": categories[0]._id
+      },
+      {
+        "snippetText": `
+        <body>
+  <!-- Bootstrap provides ready to use classes to style elements -->
+  <main class="d-flex min-vh-100 bg-dark">
+    <div class="container-fluid my-auto">
+      <h1 class="display-2 text-center text-light">Cards</h1>
+      <div class="row justify-content-center">
+</main>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+       
+        `,
+        "snippetAuthor":"Sean",
+        "category": categories[0]._id
       }
       ]
       console.log(snippetSeeds)
